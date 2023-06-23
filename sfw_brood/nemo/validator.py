60b5d563a44ce94f87e6638f36c02bd25e9ba946
gardzielb <bartosz.g.1999@gmail.com>
2023-06-23 13:11:18 +0200

    Debugging NeMo results issue

--- sfw_brood/nemo/validator.py
+++ sfw_brood/nemo/validator.py
@@ -1,9 +1,51 @@
+import json
+
+import torch
 from torch.utils.data import DataLoader
 from torchmetrics import Accuracy, ConfusionMatrix
 
 from sfw_brood.model import ModelValidator, SnowfinchBroodClassifier
 from sfw_brood.nemo.model import SnowfinchBroodMatchboxNet
 from sfw_brood.validation import display_confusion_matrix, save_results
+
+
+@torch.no_grad()
+def extract_logits(model, dataloader):
+	logits_buffer = []
+	label_buffer = []
+
+	# Follow the above definition of the test_step
+	for batch in dataloader:
+		audio_signal, audio_signal_len, labels, labels_len = batch
+		logits = model(input_signal = audio_signal, input_signal_length = audio_signal_len)
+
+		logits_buffer.append(logits)
+		label_buffer.append(labels)
+		print(".", end = '')
+	print()
+
+	print("Finished extracting logits !")
+	logits = torch.cat(logits_buffer, 0)
+	labels = torch.cat(label_buffer, 0)
+	return logits, labels
+
+
+def parse_manifest(manifest):
+	data = []
+	for line in manifest:
+		line = json.loads(line)
+		data.append(line)
+
+	return data
+
+
+class ReverseMapLabel:
+	def __init__(self, data_loader):
+		self.label2id = dict(data_loader.dataset.label2id)
+		self.id2label = dict(data_loader.dataset.id2label)
+
+	def __call__(self, pred_idx, label_idx):
+		return self.id2label[pred_idx], self.id2label[label_idx]
 
 
 class MatchboxNetValidator(ModelValidator):
@@ -42,3 +84,20 @@ class MatchboxNetValidator(ModelValidator):
 		)
 
 		return summary
+
+		# cpu_model = model.network.cpu()
+		# cpu_model.eval()
+		# logits, labels = extract_logits(cpu_model, self.data_loader)
+		# print("Logits:", logits.shape, "Labels :", labels.shape)
+		#
+		# acc = cpu_model._accuracy(logits = logits, labels = labels)
+		# print("Accuracy : ", float(acc[0] * 100))
+		#
+		# rev_map = ReverseMapLabel(self.data_loader)
+		# results = []
+		#
+		# probs = torch.softmax(logits, dim = -1)
+		# probas, preds = torch.max(probs, dim = -1)
+		# total_count = cpu_model._accuracy.total_counts_k[0]
+		#
+		# print(total_count.items())
