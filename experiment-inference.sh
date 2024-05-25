79ce0dc717d6c442b2584d163a3013e7d6aa4944
gardzielb <bartosz.g.1999@gmail.com>
2024-05-25 10:40:17 +0200

    Improved inference experiment script

--- experiment-inference.sh
+++ experiment-inference.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-./venv/bin/python -m sfw_brood.experiment.test_inference -t $2 $1 > experiment-inference.log 2>&1 &
+if [ "$#" -eq 4 ]; then
+  ./venv/bin/python -m sfw_brood.experiment.test_inference -t $2 -p $3 -o $4 $1 > experiment-inference.log 2>&1 &
+else
+  ./venv/bin/python -m sfw_brood.experiment.test_inference -t $2 $1 > experiment-inference.log 2>&1 &
+fi
